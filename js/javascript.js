5c7c353dbaa1f69b7a16fbc045327b673e78af19
reaji <R@douBles-MacBook-Pro.local>
2017-02-22 20:53:41 -0800

    It is draggable now!!!!!

--- js/javascript.js
+++ js/javascript.js
@@ -18,60 +18,81 @@ $(document).ready(function(){
 // 	}
 // });
 
+$('.droughtones').draggable({ cursor: "move", cursorAt: { top: 300, left: 100 } });
+
 
 $(window).scroll(function () {
         if ($(this).scrollTop() > -150 && $(this).scrollTop() < 500) {
             $('#r2009').css( "opacity", "1" );
+            $('#r2009').css( "z-index", "2" );
+            // $('.droughtones').css( "top", mouseY );
         } else {
             $('#r2009').css( "opacity", "0" );
+            $('#r2009').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 500 && $(this).scrollTop() < 1000) {
             $('#r2010').css( "opacity", "1" );
+            $('#r2010').css( "z-index", "2" );
         } else {
             $('#r2010').css( "opacity", "0" );
+            $('#r2010').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 1000 && $(this).scrollTop() < 1500) {
             $('#r2011').css( "opacity", "1" );
+            $('#r2011').css( "z-index", "2" );
         } else {
             $('#r2011').css( "opacity", "0" );
+            $('#r2011').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 1500 && $(this).scrollTop() < 2000) {
             $('#r2012').css( "opacity", "1" );
+            $('#r2012').css( "z-index", "2" );
         } else {
             $('#r2012').css( "opacity", "0" );
+            $('#r2012').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 2000 && $(this).scrollTop() < 2500) {
             $('#r2013').css( "opacity", "1" );
+            $('#r2013').css( "z-index", "2" );
         } else {
             $('#r2013').css( "opacity", "0" );
+            $('#r2013').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 2500 && $(this).scrollTop() < 3000) {
             $('#r2014').css( "opacity", "1" );
+            $('#r2014').css( "z-index", "2" );
         } else {
             $('#r2014').css( "opacity", "0" );
+            $('#r2014').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 3000 && $(this).scrollTop() < 3500) {
             $('#r2015').css( "opacity", "1" );
+            $('#r2015').css( "z-index", "2" );
         } else {
             $('#r2015').css( "opacity", "0" );
+            $('#r2015').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 3500 && $(this).scrollTop() < 4000) {
             $('#r2016').css( "opacity", "1" );
+            $('#r2016').css( "z-index", "2" );
         } else {
             $('#r2016').css( "opacity", "0" );
+            $('#r2016').css( "z-index", "0" );
         }
 
         if ($(this).scrollTop() > 4000 && $(this).scrollTop() < 4500) {
             $('#r2017').css( "opacity", "1" );
+            $('#r2017').css( "z-index", "2" );
         } else {
             $('#r2017').css( "opacity", "0" );
+            $('#r2017').css( "z-index", "0" );
         }
     });
 
