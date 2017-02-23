198d5fab5b542d81afaa34c8d93bd06e0e352727
reaji <R@douBles-MacBook-Pro.local>
2017-02-22 18:54:54 -0800

    shadow

--- js/javascript.js
+++ js/javascript.js
@@ -76,12 +76,12 @@ $(window).scroll(function () {
     });
 
 
-$(window).scroll(_.debounce(function(){
-    $('#navbackground').css("box-shadow", "rgba(0,0,0,0.16) 0 2px 4px");
-}, 150, { 'leading': true, 'trailing': false }));
+// $(window).scroll(_.debounce(function(){
+//     $('#navbackground').css("box-shadow", "rgba(0,0,0,0.16) 0 2px 4px");
+// }, 150, { 'leading': true, 'trailing': false }));
 
-$(window).scroll(_.debounce(function(){
-    $('#navbackground').css("box-shadow", "none");
-}, 150));
+// $(window).scroll(_.debounce(function(){
+//     $('#navbackground').css("box-shadow", "none");
+// }, 150));
 
 });
